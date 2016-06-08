464cca46cec68354c352c3af21242b2c32cdf51f
Vitor Oliveira <vbrazo@gmail.com>
2016-06-08 01:18:06 -0300

    Adds custom collectionViewCell to display the results

---
+++ discount_ascii_warehouse/Grid.swift
@@ -0,0 +1,17 @@
+//
+//  Grid.swift
+//  discount_ascii_warehouse
+//
+//  Created by Vitor Oliveira on 6/1/16.
+//  Copyright © 2016 Vitor Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class CollectionsGrid: UICollectionViewCell {
+    
+    @IBOutlet weak var lblFace: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var btnBuyNow: UIButton!
+    
+}
