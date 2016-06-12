ca12c4cb54bf64c45b97078f9ba4ac504a985995
Vitor Oliveira <vbrazo@gmail.com>
2016-06-11 23:20:17 -0300

    Adds test source files to the project

---
+++ discount_ascii_warehouseTests/ModelsTest.swift
@@ -0,0 +1,39 @@
+//
+//  discount_ascii_warehouseTests.swift
+//  discount_ascii_warehouseTests
+//
+//  Created by Vitor Oliveira on 6/11/16.
+//  Copyright © 2016 Vitor Oliveira. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import CoreData
+
+class discount_ascii_warehouseTests: XCTestCase {
+    
+    var coreDataStack: CoreDataStack!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ discount_ascii_warehouseTests/TestDataCoreStack.swift
@@ -0,0 +1,9 @@
+//
+//  TestDataCoreStack.swift
+//  discount_ascii_warehouse
+//
+//  Created by Vitor Oliveira on 6/11/16.
+//  Copyright © 2016 Vitor Oliveira. All rights reserved.
+//
+
+import Foundation
